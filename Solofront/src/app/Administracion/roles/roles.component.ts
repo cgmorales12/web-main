83df69970a23222cac026745e6420f2a46a3def0
cgmorales12 <cgmorales1280@gmail.com>
2025-09-07 09:41:59 -0500

    Add roles CRUD listing and creation

--- Solofront/src/app/Administracion/roles/roles.component.ts
+++ Solofront/src/app/Administracion/roles/roles.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RolService } from '../../Services/rol.service';
+import { IRol } from '../../interfases/irol';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './roles.component.html',
-  styleUrl: './roles.component.css'
+  styleUrl: './roles.component.css',
 })
 export class RolesComponent {
+  roles: IRol[] = [];
+  nuevoRol: IRol = { nombre: '' };
 
+  constructor(private rolService: RolService) {}
+
+  ngOnInit() {
+    this.cargarRoles();
+  }
+
+  cargarRoles() {
+    this.rolService.getRoles().subscribe((roles) => (this.roles = roles));
+  }
+
+  agregarRol() {
+    if (!this.nuevoRol.nombre.trim()) {
+      return;
+    }
+    this.rolService
+      .addRole({ nombre: this.nuevoRol.nombre })
+      .subscribe(() => {
+        this.nuevoRol.nombre = '';
+        this.cargarRoles();
+      });
+  }
 }
